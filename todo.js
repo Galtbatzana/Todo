7b9f41c40521797d0165df83278694c587a2fc31
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-06-27 15:46:14 +0800

    last 0627

--- todo.js
+++ todo.js
@@ -1,30 +1,91 @@
+///////////////////////////////////////////////////////
+// 
+// const tasks = [
+//     {name: "My todo list #1", status: "todo"},
+//     {name: "wash dishes", status: "todo"},
+//     {name: "watch football", status: "done"} 
+// ];
 
-
+const tasksString = localStorage.getItem("Tasks");
+const tasks = JSON.parse(tasksString) || [];
+//////////////////////////////////////////////////////////
 function addTask() {
-    const input = document.getElementById("input");
-    console.log(input.value);
-    // constent.innerHTML = content.innerHTML + "<div>" + input.value + "</div>";   
+    const newTask = document.getElementById("input").value;
+
+    if (newTask === "") {
+        alert("Enter task in the input");
+        return;
+    } 
+    tasks.unshift({name: newTask, status: "todo"});
+    document.getElementById("input").value = "";
+    render(); 
 }
-addTask();
 
+////////////////////////////////////////////////////////
+function render() {
+    let tasksHtml = "";
 
-
-function editText() {
-    todoText = "";
-    const todoText = document.getElementById("title");
+for (let i = 0; i < tasks.length; i++) {
     
+    const oneTask = `
+        <div class="body-row">
+            
+            <input class="body-check-box" type="checkbox" onclick="changeStatus(${i});" ${tasks[i].status === "done" ? "checked" : ""} />
+            ${tasks[i].status === "done" ? `<s>${tasks[i].name}</s>` : tasks[i].name}
+            
+            <div id="cart-content"></div>
+            <button class="btn-edit" onclick="editTask(${i});">edit</button>
+            <button class="btn-delete" onclick="delText(${i});">del</button>
+          </div>
+    `;
+    tasksHtml = tasksHtml + oneTask;  
+}
+document.getElementById("tasks").innerHTML = tasksHtml;
+
+let doneCount = 0;
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].status === "done") {
+            doneCount++;
+        }
+    }
+    document.getElementById("statistics").innerText = `${doneCount}/${tasks.length}`;
+    
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+}
+render();
+
+/////////////////////////////////////////////////////////
+function delText(position) {
+   if(confirm("Are you sure?")) {
+    tasks.splice(position, 1);
+    render();
+   }
 }
 
-function delText() {
-    emptyText = "";
-    const todoText = document.getElementById("title");
-    console.log(todoText.innerText);
-    const emptyText = todoText;
-   
+///////////////////////////////////////////////////////
+function addOnEnter(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+}  
+
+////////////////////////////////////////////////////////
+function editTask(position) {
+   const oldValue = tasks[position].name;
+   const newValue = prompt("EditTask", oldValue);
+   if (newValue !== null) {
+        tasks[position].name = newValue;
+    }
+    render();
 }
 
-function addText() {
-    const titleText = document.getElementById("title");
-    titleText.innerText = input.value;
-    console.log(titleText.innerText);
+//////////////////////////////////////////////////////////
+function changeStatus(position) {
+    if (tasks[position].status === "done") {
+        tasks[position].status = "todo";
+    } else {
+        tasks[position].status = "done";
+    }
+   render();
 }
+
